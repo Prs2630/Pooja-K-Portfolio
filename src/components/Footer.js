c51dfffc75f4d3884f883199334fee172180428c
Prs2630 <poojakinholkar1998@gmail.com>
2022-07-27 17:04:47 +0530

    added logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Pooja Kinholkar</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} Poojs's</h3>
+          <h3>Copyright © {year} Pooja's</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
